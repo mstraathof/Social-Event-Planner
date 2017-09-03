bd128fd62ba06c912387048a5eaa4160c396e839
unknown <jnm219@lehigh.edu>
2017-09-03 17:58:11 -0400

    added a listview to display data from the network

--- android/tutorialforjnm219/app/src/main/java/jnm219/cse216/lehigh/edu/tutorialforjnm219/MainActivity.java
+++ android/tutorialforjnm219/app/src/main/java/jnm219/cse216/lehigh/edu/tutorialforjnm219/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,6 +18,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,17 +42,32 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("mfs409", "Response is: " + response);
+                        final ArrayList<String> myList = new ArrayList<>();
+                        try {
+                            JSONArray jStringArray = new JSONArray(response);
+                            for (int i = 0; i < jStringArray.length(); ++i) {
+                                myList.add(jStringArray.getString(i));
+                            }
+                        } catch (final JSONException e) {
+                            Log.d("mfs409", "Error parsing JSON file..." + e.getMessage());
+                        }
+                        ListView mListView = (ListView) findViewById(R.id.datum_list_view);
+                        ArrayAdapter adapter = new ArrayAdapter<>(MainActivity.this,
+                                android.R.layout.simple_list_item_1,
+                                myList);
+                        mListView.setAdapter(adapter);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("mfs409", "That didn't work!");
+                Log.e("jnm219", "That didn't work!");
             }
         });
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+
+
 
     }
 
@@ -70,4 +92,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
