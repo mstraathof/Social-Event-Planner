526356e2c391c89c026242b06d41d62af5ce6db9
unknown <jnm219@lehigh.edu>
2017-09-18 22:37:48 -0400

    Added modal, still need to add edit button functionality

--- web/app.ts
+++ web/app.ts
@@ -1,31 +1,28 @@
 /// <reference path="ts/EditEntryForm.ts"/>
 /// <reference path="ts/NewEntryForm.ts"/>
 /// <reference path="ts/ElementList.ts"/>
+/// <reference path="ts/Navbar.ts"/>
+
+// Prevent compiler errors when using jQuery.  "$" will be given a type of 
+// "any", so that we can use it anywhere, and assume it has any fields or
+// methods, without the compiler producing an error.
+var $: any;
 
 // Prevent compiler errors when using Handlebars
 var Handlebars: any;
 
+// a global for the EditEntryForm of the program.  See newEntryForm for 
+// explanation
+var editEntryForm: EditEntryForm;
+
 // Run some configuration code when the web page loads
 $(document).ready(function () {
-    // Create the object that controls the "New Entry" form
-    newEntryForm = new NewEntryForm();
-    // Create the object for the main data list, and populate it with data from
-    // the server
-    mainList = new ElementList();
+    Navbar.refresh();
+    NewEntryForm.refresh();
+    ElementList.refresh();
 
     // Create the object that controls the "Edit Entry" form
     editEntryForm = new EditEntryForm();
-
-    // Populate the Element List Singleton with data from the server
-    ElementList.refresh();
-    
     // set up initial UI state
     $("#editElement").hide();
-    $("#addElement").hide();
-    $("#showElements").show();
-    // set up the "Add Message" button
-    $("#showFormButton").click(function () {
-        $("#addElement").show();
-        $("#showElements").hide();
-    });
 });
--- web/ts/NewEntryForm.ts
+++ web/ts/NewEntryForm.ts
@@ -1,82 +1,108 @@
-// Prevent compiler errors when using jQuery.  "$" will be given a type of 
-// "any", so that we can use it anywhere, and assume it has any fields or
-// methods, without the compiler producing an error.
-var $: any;
-
-// The 'this' keyword does not behave in JavaScript/TypeScript like it does in
-// Java.  Since there is only one NewEntryForm, we will save it to a global, so
-// that we can reference it from methods of the NewEntryForm in situations where
-// 'this' won't work correctly.
-var newEntryForm: NewEntryForm;
-
 /**
  * NewEntryForm encapsulates all of the code for the form for adding an entry
  */
 class NewEntryForm {
-    /**
-     * To initialize the object, we say what method of NewEntryForm should be
-     * run in response to each of the form's buttons being clicked.
-     */
-    constructor() {
-        $("#addCancel").click(this.clearForm);
-        $("#addButton").click(this.submitForm);
-    }
-
-    /**
-     * Clear the form's input fields
-     */
-    clearForm() {
-        $("#newTitle").val("");
-        $("#newMessage").val("");
-        // reset the UI
-        $("#addElement").hide();
-        $("#editElement").hide();
-        $("#showElements").show();
-    }
-
-    /**
-     * Check if the input fields are both valid, and if so, do an AJAX call.
-     */
-    submitForm() {
-        // get the values of the two fields, force them to be strings, and check 
-        // that neither is empty
-        let title = "" + $("#newTitle").val();
-        let msg = "" + $("#newMessage").val();
-        if (title === "" || msg === "") {
-            window.alert("Error: title or message is not valid");
-            return;
+    
+        /**
+         * The name of the DOM entry associated with NewEntryForm
+         */
+        private static readonly NAME = "NewEntryForm";
+    
+        /**
+         * Track if the Singleton has been initialized
+         */
+        private static isInit = false;
+    
+        /**
+         * Initialize the NewEntryForm by creating its element in the DOM and 
+         * configuring its buttons.  This needs to be called from any public static 
+         * method, to ensure that the Singleton is initialized before use
+         */
+        private static init() {
+            if (!NewEntryForm.isInit) {
+                $("body").append(Handlebars.templates[NewEntryForm.NAME + ".hb"]());
+                $("#" + NewEntryForm.NAME + "-OK").click(NewEntryForm.submitForm);
+                $("#" + NewEntryForm.NAME + "-Close").click(NewEntryForm.hide);
+                NewEntryForm.isInit = true;
+            }
         }
-        // set up an AJAX post.  When the server replies, the result will go to
-        // onSubmitResponse
-        $.ajax({
-            type: "POST",
-            url: "/messages",
-            dataType: "json",
-            data: JSON.stringify({ mTitle: title, mMessage: msg }),
-            success: newEntryForm.onSubmitResponse
-        });
+    
+        /**
+         * Refresh() doesn't really have much meaning, but just like in sNavbar, we
+         * have a refresh() method so that we don't have front-end code calling
+         * init().
+         */
+        public static refresh() {
+            NewEntryForm.init();
+        }
+    
+        /**
+         * Hide the NewEntryForm.  Be sure to clear its fields first
+         */
+        private static hide() {
+            $("#" + NewEntryForm.NAME + "-title").val("");
+            $("#" + NewEntryForm.NAME + "-message").val("");
+            $("#" + NewEntryForm.NAME).modal("hide");
+        }
+    
+        /**
+         * Show the NewEntryForm.  Be sure to clear its fields, because there are
+         * ways of making a Bootstrap modal disapper without clicking Close, and
+         * we haven't set up the hooks to clear the fields on the events associated
+         * with those ways of making the modal disappear.
+         */
+        public static show() {
+            $("#" + NewEntryForm.NAME + "-title").val("");
+            $("#" + NewEntryForm.NAME + "-message").val("");
+            $("#" + NewEntryForm.NAME).modal("show");
+        }
+    
+    
+        /**
+         * Send data to submit the form only if the fields are both valid.  
+         * Immediately hide the form when we send data, so that the user knows that 
+         * their click was received.
+         */
+        private static submitForm() {
+            // get the values of the two fields, force them to be strings, and check 
+            // that neither is empty
+            let title = "" + $("#" + NewEntryForm.NAME + "-title").val();
+            let msg = "" + $("#" + NewEntryForm.NAME + "-message").val();
+            if (title === "" || msg === "") {
+                window.alert("Error: title or message is not valid");
+                return;
+            }
+            NewEntryForm.hide();
+            // set up an AJAX post.  When the server replies, the result will go to
+            // onSubmitResponse
+            $.ajax({
+                type: "POST",
+                url: "/messages",
+                dataType: "json",
+                data: JSON.stringify({ mTitle: title, mMessage: msg }),
+                success: NewEntryForm.onSubmitResponse
+            });
+        }
+    
+        /**
+         * onSubmitResponse runs when the AJAX call in submitForm() returns a 
+         * result.
+         * 
+         * @param data The object returned by the server
+         */
+        private static onSubmitResponse(data: any) {
+            // If we get an "ok" message, clear the form and refresh the main 
+            // listing of messages
+            if (data.mStatus === "ok") {
+                ElementList.refresh();
+            }
+            // Handle explicit errors with a detailed popup message
+            else if (data.mStatus === "error") {
+                window.alert("The server replied with an error:\n" + data.mMessage);
+            }
+            // Handle other errors with a less-detailed popup message
+            else {
+                window.alert("Unspecified error");
+            }
+        }
     }
-
-    /**
-     * onSubmitResponse runs when the AJAX call in submitForm() returns a 
-     * result.
-     * 
-     * @param data The object returned by the server
-     */
-    private onSubmitResponse(data: any) {
-        // If we get an "ok" message, clear the form
-        if (data.mStatus === "ok") {
-            newEntryForm.clearForm();
-            ElementList.refresh();
-        }
-        // Handle explicit errors with a detailed popup message
-        else if (data.mStatus === "error") {
-            window.alert("The server replied with an error:\n" + data.mMessage);
-        }
-        // Handle other errors with a less-detailed popup message
-        else {
-            window.alert("Unspecified error");
-        }
-        ElementList.refresh();
-    }
-} // end class NewEntryForm
