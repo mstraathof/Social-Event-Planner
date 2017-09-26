8bfb654ff708c54f8b563ee97d2b9d582a454121
Joon Seonwoo <jos219@lehigh.edu>
2017-09-26 01:25:09 -0400

    Changed my test so that I leave a test table running for other people to test it on, and I changed a comment in App.java

--- admin/admin-cli/src/test/java/edu/lehigh/cse216/jnm219/admin/DatabaseTest.java
+++ admin/admin-cli/src/test/java/edu/lehigh/cse216/jnm219/admin/DatabaseTest.java
@@ -36,13 +36,24 @@ public class DatabaseTest
         assertTrue( true );
     }
     /**
+     * Making sure I get disconnected to the database
+     */
+    public void testDisconnect()
+    {
+        Database db = Database.getDatabase("jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com:5432/dd8h04ocdonsvj?user=qcxhljggghpbxa&password=6d462cf3d5d52813f0a69912a10908fad2ff06725737ce41e0cf0750b83d2375&sslmode=require");
+        assertTrue(db.disconnect());
+       
+    }
+    /**
      * Test creating the table, if it creates an extra, it should return false
      */
     public void testCreateTable()
     {
-        Database db = Database.getDatabase( "jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com:5432/dd8h04ocdonsvj?user=qcxhljggghpbxa&password=6d462cf3d5d52813f0a69912a10908fad2ff06725737ce41e0cf0750b83d2375&sslmode=require");
+        Database db = Database.getDatabase("jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com:5432/dd8h04ocdonsvj?user=qcxhljggghpbxa&password=6d462cf3d5d52813f0a69912a10908fad2ff06725737ce41e0cf0750b83d2375&sslmode=require");
+        db.dropTable();
         assertTrue(db.createTable());
         assertFalse(db.createTable());
+        db.disconnect();
     }
     /**
      * Test dropping the table, if it drops nothing, it should return false
@@ -52,14 +63,8 @@ public class DatabaseTest
         Database db = Database.getDatabase ("jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com:5432/dd8h04ocdonsvj?user=qcxhljggghpbxa&password=6d462cf3d5d52813f0a69912a10908fad2ff06725737ce41e0cf0750b83d2375&sslmode=require");
         assertTrue(db.dropTable());
         assertFalse(db.dropTable());
+        db.createTable();
+        db.disconnect();
     }
-    /**
-     * Making sure I get disconnected to the database
-     */
-    public void testDisconnect()
-    {
-        Database db = Database.getDatabase( "jdbc:postgresql://ec2-107-22-211-182.compute-1.amazonaws.com:5432/dd8h04ocdonsvj?user=qcxhljggghpbxa&password=6d462cf3d5d52813f0a69912a10908fad2ff06725737ce41e0cf0750b83d2375&sslmode=require");
-        assertTrue(db.disconnect());
-       
-    }
+    
 }
